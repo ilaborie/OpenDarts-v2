6986d25fe1a8ba3787f5efea96519442d0e1825a
Igor Laborie <ilaborie@gmail.com>
2012-10-03 18:34:29 +0200

    Prepare for multi could

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,15 @@
 #!/bin/bash
+
+# Heroku
 git push heroku master
+
+# CloudBees
+# git push cloudbees master 
+
+# Jelastic
+
+# Google App Engine
+
+# CouldFoundry
 play dist
 vmc update opendarts --path=dist/opendarts-2.0-SNAPSHOT.zip
