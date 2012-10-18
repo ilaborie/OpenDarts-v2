8ec4e192b370657ea7b73065829290c50bd23ec6
Igor Laborie <ilaborie@gmail.com>
2012-10-18 08:46:02 +0200

    Fix deployment on Cloudbees

--- deploy.sh
+++ deploy.sh
@@ -3,25 +3,33 @@
 DIST_PATH=dist/opendarts-2.0-SNAPSHOT.zip
 
 # Build application
+echo "Build Application"
 rm $DIST_PATH
 play dist
 
 # Github
+echo "Push to Github"
 git push origin master
 git push origin develop
 
 # Deploy on Heroku
+echo "Push & Deploy to Heroku"
 git push heroku master
 
 # Deploy on CouldFoundry
+echo "Deploy on CouldFoundry"
 vmc update opendarts --path=$DIST_PATH
 
 # Deploy on CloudBees
+echo "Push & Deploy to CloudBees"
 git push cloudbees master 
-#bees app:deploy -ep eu -a ilaborie/opendarts2-2 -t play2 $DIST_PATH
-play cloudbees-deploy
+bees app:deploy -a ilaborie/opendarts2-2 -t play2 $DIST_PATH
 
 # TODO Deploy onJelastic
 
 # TODO Google App Engine
 
+## Links
+echo "Heroku: http://opendarts.herokuapp.com/"
+echo "CouldFoundry: http://opendarts.cloudfoundry.com/"
+echo "CloudBees: http://opendarts2-2.ilaborie.cloudbees.net/"
