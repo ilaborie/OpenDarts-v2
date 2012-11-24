0d0a10e8ff2a05befa161928a63e358c3b621ec9
Igor Laborie <ilaborie@gmail.com>
2012-11-24 13:35:48 +0100

    Script for easier Jelastic deploiement

--- deploy.sh
+++ deploy.sh
@@ -7,21 +7,40 @@ rm $DIST_PATH
 play dist
 
 # Github
+# https://github.com/ilaborie/OpenDarts-v2
+echo "Push to Github"
 git push origin master
 git push origin develop
 
-# Deploy on Heroku
+# https://dashboard.heroku.com/apps/opendarts
+echo "Push & Deploy to Heroku"
 git push heroku master
 
 # Deploy on CouldFoundry
+# http://blog.cloudfoundry.com/2012/05/31/cloud-foundry-now-supports-play/
+echo "Deploy on CouldFoundry (need a vmc login before)"
 vmc update opendarts --path=$DIST_PATH
 
 # Deploy on CloudBees
+# https://run.cloudbees.com/a/ilaborie#app-manage/development:ilaborie/opendarts2-2
+echo "Push & Deploy to CloudBees"
 git push cloudbees master 
-#bees app:deploy -ep eu -a ilaborie/opendarts2-2 -t play2 $DIST_PATH
 play cloudbees-deploy
 
-# TODO Deploy onJelastic
+# Build for Jelastic
+# https://app.jelastic.dogado.eu/
+echo "Build for Jelastic"
+rm target/*.war
+play package
+mv target/opendarts*.war target/ROOT.war
 
 # TODO Google App Engine
+
+# TODO OpenShift https://github.com/opensas/play2-openshift-quickstart#readme
+
+## Links
+echo "Heroku: http://opendarts.herokuapp.com/"
+echo "CouldFoundry: http://opendarts.cloudfoundry.com/"
+echo "CloudBees: http://opendarts2-2.ilaborie.cloudbees.net/"
+echo "Jelastic: should deploy manualy at https://app.jelastic.dogado.eu/"
 
--- project/plugins.sbt
+++ project/plugins.sbt
@@ -4,6 +4,9 @@ logLevel := Level.Warn
 // The Typesafe repository
 resolvers += "Typesafe repository" at "http://repo.typesafe.com/typesafe/releases/"
 
+// The Play2war repository
+resolvers += "Play2war plugins release" at "http://repository-play-war.forge.cloudbees.com/release/"
+
 // The Sonatype OSS repository
 resolvers += "Sonatype OSS Snasphots" at "https://oss.sonatype.org/content/repositories/snapshots"
 
@@ -12,3 +15,7 @@ addSbtPlugin("play" % "sbt-plugin" % "2.0.4")
 
 // Use the Cloudbees plugin
 addSbtPlugin("com.cloudbees.deploy.play" % "sbt-cloudbees-play-plugin" % "0.3")
+
+// Use Play War Plugin for WAR building
+// see https://github.com/dlecan/play2-war-plugin
+addSbtPlugin("com.github.play2war" % "play2-war-plugin" % "0.8.1")
